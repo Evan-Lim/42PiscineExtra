1a26655a0ff8c44d5c1df22eb90f90b649e8ff51
Evan-Lim <evanlim804@gmail.com>
2026-08-24 21:32:00 +0800

    first 5 exercises

---
+++ Level01/maff_alpha.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int	main(void)
+{
+	int	i;
+	int	c;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		if (i % 2 == 0)
+		{
+			c = i - 32;
+			write(1, &c, 1);
+		}
+		else
+			write(1, &i, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
