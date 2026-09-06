d449734bf5f14a7e85e1c680251b930c607a9b91
Evan-Lim <evanlim804@gmail.com>
2026-09-06 12:01:30 +0800

    repeat_alpha

--- Level01/repeat_alpha.c
+++ Level01/repeat_alpha.c
@@ -10,11 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		
+		int	i;
+
+		i = 0;
+		while (argv[1][i])
+		{
+			int	repeat;
+
+			repeat = 1;
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				repeat = argv[1][i] - 'a' + 1;
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				repeat = argv[1][i] - 'A' + 1;
+			while (repeat--)
+				write(1, &argv[1][i], 1);
+			i++;
+		}
 	}
+	write(1, "\n", 1);
 	return (0);
 }
