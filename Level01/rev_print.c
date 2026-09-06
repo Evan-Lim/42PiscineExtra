64bbcf3711302aaa402427338b4b588dfd1090b9
Evan-Lim <evanlim804@gmail.com>
2026-09-06 12:04:57 +0800

    rev_print

--- Level01/rev_print.c
+++ Level01/rev_print.c
@@ -10,3 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		int	len;
+
+		len = 0;
+		while (argv[1][len])
+			len++;
+		while (--len >= 0)
+			write(1, &argv[1][len], 1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
