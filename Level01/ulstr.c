04a3346d97a0bed68e9c9dba80f2e9bf5690b756
Evan-Lim <evanlim804@gmail.com>
2026-09-06 12:16:07 +0800

    ulstr

--- Level01/ulstr.c
+++ Level01/ulstr.c
@@ -10,3 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		int	i;
+
+		i = 0;
+		while (argv[1][i])
+		{
+			char	c;
+
+			c = argv[1][i];
+			if (c >= 'a' && c <= 'z')
+				c -= 32;
+			else if (c >= 'A' && c <= 'Z')
+				c += 32;
+			write(1, &c, 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
